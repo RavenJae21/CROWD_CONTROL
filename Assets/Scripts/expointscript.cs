ab5d68588a046bd52f8fedc479c69e6d3acd0b42
ahjorlei <ahjorlei@stedwards.edu>
2025-03-11 12:16:43 -0500

    expoint success

--- Assets/Scripts/expointscript.cs
+++ Assets/Scripts/expointscript.cs
@@ -2,21 +2,24 @@ using UnityEngine;
 
 //public class expointscript : MonoBehaviour
 public class CollisionHandler : MonoBehaviour
+
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+  
+    public bool hitHotdog;
     void OnCollisionEnter(Collision collision)
     {
-        Destroy(collision.gameObject)
-    }
-
-if (collision.gameObject.tag == "Hotdog")
-{
-    hitHotdog = true;
-    Destroy(CollisionHandler.gameObject)
-}
-    // Update is called once per frame
-    void Update()
-    {
+        if (collision.gameObject.tag == "Hotdog")
+        {
+            hitHotdog = true;
+            Destroy(gameObject);
+        }
+        
         
     }
+
 }
+    
+
+
+
